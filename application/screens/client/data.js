eba3a831337ed11aedcb5ba1bfe3dcc152bd5aaf
Bdair2002 <107470382+Bdair2002@users.noreply.github.com>
2025-02-06 09:18:54 +0200

    Update data.js

--- application/screens/client/data.js
+++ application/screens/client/data.js
@@ -4,7 +4,7 @@ export const AzureOpenAIConfig = {
   apiVersion: "2024-02-01",
   endpoint: "https://public-api.devexpress.com/demo-openai",
   apiKey:
-    "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",
+    "",
 };
 export const REGENERATION_TEXT = "Regeneration...";
 export const CHAT_DISABLED_CLASS = "dx-chat-disabled";
